93a945354b4940d2752dc19e2f234f5fddc773c9
steenbergh <pim.steenbergh@zrt.nl>
2024-12-09 11:58:00 +0100

    Update day05_2.py

--- day05_2.py
+++ day05_2.py
@@ -1,49 +1,15 @@
 from day05_data import data
 
-def check(d_restrict, guide):
-  for ix, nr in enumerate(guide):
-    if nr not in d_restrict.keys():
-      continue
-    for y in range(ix):
-      if guide[y] in d_restrict[nr]:
-        return False
-  return True
-
-def repair(d_restrict, guide):
-  is_okay = False
-  iter = 0
-  while not is_okay:
-    #print(iter)
-    for ix, nr in enumerate(guide):
-      if nr not in d_restrict.keys():
-        continue
-      for y in range(ix):
-        if guide[y] in d_restrict[nr]:
-          tmp = guide.pop(y)
-          guide.append(tmp)
-    is_okay = check(d_restrict, guide)
-    iter += 1
-  return guide
-
 data = data.split('\n')
 data_restrict = [d for d in data if '|' in d]
 data_pages = [d for d in data if ',' in d]
 
-dict_restrict = {}
+d_beforeme = {}
 for dr in data_restrict:
-  pcs = [int(z) for z in dr.split('|')]
-  if pcs[0] not in dict_restrict:
-    dict_restrict[pcs[0]] = [pcs[1]]
+  pcs = dr.split('|')
+  if pcs[1] in d_beforeme:
+    d_beforeme[pcs[1]].append(pcs[0])
   else:
-    dict_restrict[pcs[0]].append(pcs[1])
+    d_beforeme[pcs[1]] = [pcs[0]]
 
-middle_sums = 0
-safety_guides = [[int(nr) for nr in page.split(',')] for page in data_pages]
-for g in safety_guides:
-  if check(dict_restrict, g):
-    middle_sums += g[len(g)//2]
-  else:
-    g = repair(dict_restrict, g)
-    middle_sums += g[len(g)//2]
-
-print(middle_sums)
+print(d_beforeme)
