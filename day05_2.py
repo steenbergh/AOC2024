2f0acbd50a1e20b2a6bc7b844c7171b1710ac737
steenbergh <pim.steenbergh@zrt.nl>
2024-12-09 16:23:18 +0100

    Update day05_2.py

--- day05_2.py
+++ day05_2.py
@@ -1,4 +1,4 @@
-from day05_data import data
+#from day05_data import data
 
 data = data.split('\n')
 data_restrict = [d for d in data if '|' in d]
@@ -6,10 +6,25 @@ data_pages = [d for d in data if ',' in d]
 
 d_beforeme = {}
 for dr in data_restrict:
-  pcs = dr.split('|')
+  pcs = [int(x) for x in dr.split('|')]
+
   if pcs[1] in d_beforeme:
     d_beforeme[pcs[1]].append(pcs[0])
   else:
     d_beforeme[pcs[1]] = [pcs[0]]
 
-print(d_beforeme)
+# Acceptable Order
+ao = []
+while len(d_beforeme) > 0:
+  for x in range(10, 100):
+    if x in ao: continue         # already accepted
+    if x in d_beforeme: continue # we still have reservations about this number
+    for bm in d_beforeme.keys():
+      for ix, page in enumerate(d_beforeme[bm]):
+        if page==x:
+          del(d_beforeme[bm][ix])
+          break
+        if len(d_beforeme[bm]) == 0:
+          del(d_beforeme[bm])
+    ao.append(x)
+    print(ao)
