ed0520cc15923a3c6ef4cb59725c0481902c5248
steenbergh <pim.steenbergh@zrt.nl>
2024-12-11 15:25:55 +0100

    Update day05_2.py

--- day05_2.py
+++ day05_2.py
@@ -1,30 +1,38 @@
-#from day05_data import data
+from day05_data import data
+
+def diagnose(d_restrict, guide):
+  for ix, nr in enumerate(guide):
+    if nr not in d_restrict.keys():
+      continue
+    for y in range(ix):
+      if guide[y] in d_restrict[nr]:
+        #print(f'cant have {guide[y]} before {nr}, {guide} is invalid!')
+        return (y, ix)
+  return [-1]
 
 data = data.split('\n')
 data_restrict = [d for d in data if '|' in d]
 data_pages = [d for d in data if ',' in d]
 
-d_beforeme = {}
+dict_restrict = {}
 for dr in data_restrict:
-  pcs = [int(x) for x in dr.split('|')]
-
-  if pcs[1] in d_beforeme:
-    d_beforeme[pcs[1]].append(pcs[0])
+  pcs = [int(z) for z in dr.split('|')]
+  if pcs[0] not in dict_restrict:
+    dict_restrict[pcs[0]] = [pcs[1]]
   else:
-    d_beforeme[pcs[1]] = [pcs[0]]
+    dict_restrict[pcs[0]].append(pcs[1])
 
-# Acceptable Order
-ao = []
-while len(d_beforeme) > 0:
-  for x in range(10, 100):
-    if x in ao: continue         # already accepted
-    if x in d_beforeme: continue # we still have reservations about this number
-    for bm in d_beforeme.keys():
-      for ix, page in enumerate(d_beforeme[bm]):
-        if page==x:
-          del(d_beforeme[bm][ix])
-          break
-        if len(d_beforeme[bm]) == 0:
-          del(d_beforeme[bm])
-    ao.append(x)
-    print(ao)
+middle_sums = 0
+safety_guides = [[int(nr) for nr in page.split(',')] for page in data_pages]
+for g in safety_guides:
+  diag = diagnose(dict_restrict, g)
+  if diag[0] == -1: continue
+  while diag[0] != -1:
+    tmp = g[diag[0]]
+    g[diag[0]] = g[diag[1]]
+    g[diag[1]] = tmp
+    diag = diagnose(dict_restrict, g)  
+    
+  middle_sums += g[len(g)//2]
+
+print(middle_sums)
